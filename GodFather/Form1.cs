db4494384c3bf45563c08a0a1d0ee020ad99549b
Nick Kapinakis <nk@nikap.com>
2018-08-07 17:20:09 +0300

    Init Commit

---
+++ GodFather/Form1.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace GodFather
+{
+    public partial class Form1 : Form
+    {
+        private FileSystemWatcher _mWatcher;
+        private int _chngNumber;
+        private int _allNumber;
+        public DirectoryInfo Info;
+        public int TheTempFileCount;
+
+        public Form1()
+        {
+            InitializeComponent();
+            TheTempFileCount = 0;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (button2.Text == @"Autorename On")
+            {
+                button2.Text = @"Autorename Off";
+                StartWatching();
+            }
+            else
+            {
+                button2.Text = @"Autorename On";
+                StopWatching();
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = folderBrowserDialog1.SelectedPath;
+            }
+
+            Info = new DirectoryInfo(textBox1.Text + "\\");
+        }
+
+        private void StartWatching()
+        {
+            _chngNumber = 0;
+            _allNumber = 0;
+            SetLabel(@"Auto-Renaming active!");
+            _mWatcher = new FileSystemWatcher
+            {
+                Path = textBox1.Text + "\\",
+                Filter = "*.*",
+                NotifyFilter = NotifyFilters.LastWrite                             
+            };
+
+            _mWatcher.Changed += new FileSystemEventHandler(OnChanged);
+            _mWatcher.Created += new FileSystemEventHandler(OnChanged);
+            _mWatcher.Deleted += new FileSystemEventHandler(OnDeleted);
+            _mWatcher.Renamed += new RenamedEventHandler(OnRenamed);
+            _mWatcher.EnableRaisingEvents = true;
+        }
+        private void StopWatching()
+        {
+            TheTempFileCount = 0;
+            try
+            {
+                _mWatcher.EnableRaisingEvents = false;
+                _mWatcher.Dispose();
+                _mWatcher = null;
+            }
+            catch
+            {
+                _mWatcher?.Dispose();
+                _mWatcher = null;
+            }
+            finally
+            {
+                SetLabel($@"{_chngNumber} of {_allNumber} renamed");
+            }
+        }
+
+        private void OnChanged(object sender, FileSystemEventArgs e)
+        {
+                RenameFiles();
+        }
+
+        private int GetLastNumber()
+        {
+            var retVal = 0;
+
+            var theFiles = checkBox1.Checked
+                ? Info.GetFiles().OrderBy(p => p.Name.Trim().ToLower()).ToArray()
+                : Info.GetFiles().OrderByDescending(p => p.LastWriteTime).ToArray();
+            foreach (var file in theFiles)
+            {
+                int.TryParse(file.Name.Substring(0, 3), out var theInt);
+                retVal = retVal < theInt ? theInt : retVal;
+            }
+            _allNumber = theFiles.Length;
+            return ++retVal;
+        }
+
+        static bool IsFileLocked(FileInfo file)
+        {
+            FileStream stream = null;
+            try
+            {
+                stream = file.Open(FileMode.Open, FileAccess.ReadWrite, FileShare.None);
+            }
+            catch (IOException)
+            {
+                return true;
+            }
+            finally
+            {
+                stream?.Close();
+            }
+            return false;
+        }
+
+        private void SetLabel(String msg)
+        {
+            label1.Invoke((MethodInvoker)(() => label1.Text = msg));
+        }
+
+
+        void OnRenamed(object sender, RenamedEventArgs e)
+        {
+
+        }
+
+        void OnDeleted(object sender, FileSystemEventArgs e)
+        {
+
+        }
+
+        int GetFileCount()
+        {
+            var theFiles = checkBox1.Checked
+                ? Info.GetFiles().OrderBy(p => p.Name.Trim().ToLower()).ToArray()
+                : Info.GetFiles().OrderByDescending(p => p.LastWriteTime).ToArray();
+            return theFiles.Length;
+        }
+
+        void RenameFiles()
+         {
+
+
+            while (TheTempFileCount!=GetFileCount())
+            {
+                TheTempFileCount = GetFileCount();
+                Thread.Sleep(500);
+            }
+
+            var theFiles = checkBox1.Checked
+                ? Info.GetFiles().OrderBy(p => p.Name.Trim().ToLower()).ToArray()
+                : Info.GetFiles().OrderByDescending(p => p.LastWriteTime).ToArray();
+            foreach (var ew in theFiles)
+            {
+                int.TryParse(ew.Name.Split(' ')[0], out var theN);
+                if (theN == 0)
+                {
+                    try
+                    {
+                        var genNum = new StringBuilder();
+                        genNum.Insert(0, 1);
+                        var str = new StringBuilder(ew.Name);
+                        var theNum = GetLastNumber().ToString().PadLeft(3, '0') + " ";
+                        str.Insert(0, theNum);
+                        var newName = textBox1.Text + "\\" + str.ToString();
+                        File.Move(ew.FullName, newName);
+                        _chngNumber++;
+                        SetLabel($@"File {ew.Name} renamed");
+                    }
+                    catch (Exception)
+                    {
+                        // ignored
+                    }
+                }
+            }
+             TheTempFileCount = 0;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+          clearNames();
+        }
+
+        void clearNames()
+        {
+            var theFiles = Info.GetFiles().OrderByDescending(p => p.LastWriteTime).ToArray();
+            foreach (var file in theFiles)
+            {
+                int.TryParse(file.Name.Trim().Substring(0, file.Name.Trim().IndexOf(' ') + 1), out var theInt);
+                if (theInt > 0)
+                {
+                    var newName = textBox1.Text + "\\" + file.Name.Trim().Substring(file.Name.Trim().IndexOf(' ') + 1);
+                    File.Move(file.FullName, newName);
+                }
+            }
+        }
+
+
+        private void button4_Click_1(object sender, EventArgs e)
+        {
+            RenameFiles();
+        }
+    }
+}
